1906866062c8847d94f7a3c0458a7532b07c5213
Itamar Laredo <60240620+laredo77@users.noreply.github.com>
2021-11-10 15:12:50 +0200

    Delete ex1.h

--- ex1.h
+++
@@ -1,8 +0,0 @@
-#ifndef EX1_H_
-#define EX1_H_
-
-int is_big_endian();
-unsigned long long merge_bytes(unsigned long long x, unsigned long long int y);
-unsigned long long put_byte(unsigned long long x, unsigned char b, int i);
-
-#endif
